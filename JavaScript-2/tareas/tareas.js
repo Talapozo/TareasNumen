dda735d34547bb391c9500c3016c686e3cba170b
unknown <mcarlose2@gmail.com>
2022-07-07 23:51:47 -0300

    Se subio tarea test 1,2 y 3 para presentar

---
+++ JavaScript-2/tareas/tareas.js
@@ -0,0 +1,238 @@
+// No cambies los nombres de las funciones.
+
+function obtenerMayor(x, y) {
+  // "x" e "y" son números enteros (int).
+  // Devuelve el número más grande
+  // Si son iguales, devuelve cualquiera de los dos
+  // Tu código:
+  if (x > y) {
+    return x;
+  }
+  else if (x < y) {
+    return y;
+  }
+  else if (x === y) {
+    return (x || y);
+  }
+}
+
+function mayoriaDeEdad(edad) {
+  //Determinar si la persona según su edad puede ingresar a un evento.
+  //Si tiene 18 años ó más, devolver --> "Allowed"
+  //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+    return "Allowed";
+  }
+  else if (edad < 18) 
+    return "Not allowed";
+}
+
+function conection(status) {
+  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
+  //Cuando el estado es igual a 1, el usuario está "Online"
+  //Cuando el estado es igual a 2, el usuario está "Away"
+  //De lo contrario, presumimos que el usuario está "Offline"
+  //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+    return "Online";
+  }
+  else if (status === 2) {
+    return "Away";
+  }
+  else (status != 1 && !2); {
+    return "Offline";
+  }  
+}
+
+function saludo(idioma) {
+  // Devuelve un saludo en tres diferentes lenguajes:
+  // Si "idioma" es "aleman", devuelve "Guten Tag!"
+  // Si "idioma" es "mandarin", devuelve "Ni Hao!"
+  // Si "idioma" es "ingles", devuelve "Hello!"
+  // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
+  // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  }
+  else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  }
+  else if (idioma === "ingles") {
+    return "Hello!";
+  }
+  else (idioma != "aleman" || "mandarin" || "ingles" && undefined); 
+    return "Hola!";
+}
+
+function colors(color) {
+  //La función recibe un color. Devolver el string correspondiente:
+  //En caso que el color recibido sea "blue", devuleve --> "This is blue"
+  //En caso que el color recibido sea "red", devuleve --> "This is red"
+  //En caso que el color recibido sea "green", devuleve --> "This is green"
+  //En caso que el color recibido sea "orange", devuleve --> "This is orange"
+  //Caso default: devuelve --> "Color not found"
+  //Pista: Puedes usar el statement Switch.
+  if (color === "blue") {
+    return "This is blue";
+  }
+  else if (color === "red") {
+    return "This is red";
+  }
+  else if (color === "green") {
+    return "This is green";
+  }
+  else if (color === "orange") {
+    return "This is orange";
+  }
+  else (color != 1 && !2); {
+    return "Color not found";
+  }  
+}
+
+
+function esDiezOCinco(numero) {
+  // Devuelve "true" si "numero" es 10 o 5
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  return numero == 10 || numero ==5;
+}
+
+function estaEnRango(numero) {
+  // Devuelve "true" si "numero" es menor que 50 y mayor que 20
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  if (numero < 50 && numero >20){
+    return true;
+  }
+  else 
+  return false;
+}
+
+function esEntero(numero) {
+  // Devuelve "true" si "numero" es un entero (int/integer)
+  // Ejemplo: 0.8 -> false
+  // Ejemplo: 1 -> true
+  // Ejemplo: -10 -> true
+  // De lo contrario, devuelve "false"
+  // Pista: Puedes resolver esto usando `Math.floor`
+  // Tu código:
+ return numero%1 == 0  
+  
+}
+
+function fizzBuzz(numero) {
+  // Si "numero" es divisible entre 3, devuelve "fizz"
+  // Si "numero" es divisible entre 5, devuelve "buzz"
+  // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+  // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0) {
+    return "fizzbuzz"
+  } 
+  else if (numero % 5 == 0) {
+    return "buzz"
+  }
+  else if (numero % 3 == 0) {
+    return "fizz"
+  }
+  else {
+    return numero
+  }
+}
+
+function operadoresLogicos(num1, num2, num3) {
+  //La función recibe tres números distintos. 
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+if (num1 == 0 || num2 == 0 || num3 == 0) {
+  return "Error";
+} else if (num1 < 0 || num2 < 0 || num3 < 0) {
+  return "Hay negativos";
+} else if (num1 > num2 && num1 > num3) {
+  return "Número 1 es mayor y positivo";
+} else if (num3 > num1 && num3 > num2) {
+  num3 = num3 + 1;
+  return num3;
+}
+  return false;
+}
+
+function esVerdadero(valor) {
+  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
+  //si su valor es true y “Soy falso” si su valor es false.
+  //Escribe tu código aquí
+  if (valor) {
+    return "Soy verdadero";
+  }
+  else return "Soy falso";
+}
+
+function tieneTresDigitos(numero) {
+  //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+  //Escribe tu código aquí
+
+  let arregloNumero = String (numero);
+  return arregloNumero.length === 3;
+}
+
+// ---------- Puntos extra ----------
+
+function esPrimo(numero) {
+  // Devuelve "true" si "numero" es primo
+  // De lo contrario devuelve "falso"
+  // Pista: un número primo solo es divisible por sí mismo y por 1
+  // Pista 2: Puedes resolverlo usando un bucle `for`
+  // Nota: Los números 0 y 1 NO son considerados números primos
+  for (let index = 2; index <= 13; index++) {
+    if(numero != index && numero % index === 0){
+      return false;
+    }
+    return numero > 1;
+  }
+}
+
+function tablaDelSeis() {
+  //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
+  //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
+  //Escribe tu código aquí   
+  let tablaDelSeis = [];
+  for (let index = 0; index <= 10; index++) {
+    tablaDelSeis.push(index * 6);
+  }
+  return tablaDelSeis;
+}
+
+function doWhile(numero) {
+  //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
+  //Retornar el valor final.
+  //Usar el bucle do ... while.
+  let contador = 0;
+  do {
+    numero += 5;
+    contador++;
+  } while (contador < 8);
+  return numero;
+}
+
+// No modificar nada debajo de esta línea, sino no correrán los test.
+// --------------------------------
+
+module.exports = {
+  obtenerMayor,
+  mayoriaDeEdad,
+  conection,
+  saludo,
+  colors,
+  esDiezOCinco,
+  estaEnRango,
+  esEntero,
+  fizzBuzz,
+  operadoresLogicos,
+  esPrimo,
+  esVerdadero,
+  tablaDelSeis,
+  tieneTresDigitos,
+  doWhile
+};
---
+++ JavaScript-3/tareas/tareas.js
@@ -0,0 +1,187 @@
+// No cambies los nombres de las funciones.
+
+function devolverPrimerElemento(array) {
+  // Devuelve el primer elemento de un array
+  // Tu código:
+  return array [0];
+}
+
+function devolverUltimoElemento(array) {
+  // Devuelve el último elemento de un array
+  // Tu código:
+  return array[array.length - 1];
+}
+
+function obtenerLargoDelArray(array) {
+  // Devuelve el largo de un array
+  // Tu código:
+  return array.length;
+}
+
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+ let resultado = []
+ for (let i = 0; i < array .length; i++) {
+    resultado.push(array[i] + 1)
+ }
+ return resultado
+}
+
+function agregarItemAlFinalDelArray(array, elemento) {
+  // Añade el "elemento" al final del array
+  // y devuelve el array
+  // Tu código:
+  return array.concat(elemento);
+}
+
+function agregarItemAlComienzoDelArray(array, elemento) {
+  // Añade el "elemento" al comienzo del array
+  // y devuelve el array
+  // Pista: usa el método `.unshift`
+  // Tu código:
+  array.unshift(elemento)
+  return array
+}
+
+function dePalabrasAFrase(palabras) {
+  // "palabras" es un array de strings/cadenas
+  // Devuelve un string donde todas las palabras estén concatenadas
+  // con espacios entre cada palabra
+  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+  // Tu código:
+  let palabra = "";
+  for (let i = 0; i < palabras.length; i++) {
+    palabra += palabras[i] + " ";
+  }
+  return palabra.trim();
+}
+
+function arrayContiene(array, elemento) {
+  // Comprueba si el elemento existe dentro de "array"
+  // Devuelve "true" si está, o "false" si no está
+  // Tu código:
+  return array.includes(elemento);
+}
+
+function agregarNumeros(numeros) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Suma todos los enteros y devuelve el valor
+  // Tu código:
+ let suma = 0;
+ for (let i = 0; i < numeros.length; i++) {
+  suma += numeros[i];
+  } 
+    return suma;
+}
+
+function promedioResultadosTest(resultadosTest) {
+  // "resultadosTest" debe ser una matriz de enteros (int/integers)
+  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+  // Tu código:
+  let promedio = resultadosTest.reduce((acumulador , elemento) =>{
+    return acumulador + elemento;
+  }, 0);
+  return promedio / resultadosTest.length;   
+}
+
+function numeroMasGrande(numeros) {
+  // "numeros" debe ser una matriz de enteros (int/integers)
+  // Devuelve el número más grande
+  // Tu código:
+
+let mayor = 0;
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] > mayor) {
+    mayor = numeros[i]
+  }
+}
+return mayor
+}
+
+function cuentoElementos(arreglo){
+  //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 19.
+  //Escribe tu código aquí
+  let cont = 0;
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] > 19) {
+      cont += 1;
+    }
+  }
+  return cont;
+}
+
+function diaDeLaSemana(numeroDeDia) {
+  //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
+  //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
+  //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
+  //Escribe tu código aquí   
+  if (numeroDeDia == 1 || numeroDeDia == 7) {
+    return 'Es fin de semana';
+  }
+  else 
+      return "Es dia Laboral";
+} 
+
+function empiezaConNueve(n) {
+  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
+  //inicia con 9 y false en otro caso.
+  //Escribe tu código aquí
+  if (n.toString()[0] === "9") return true
+  else return false
+}
+
+function todosIguales(arreglo) {
+  //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
+  //retornar true, caso contrario retornar false.
+  //Escribe tu código aquí  
+  return array = arreglo.every(x => x=== arreglo[0]);
+} 
+
+function mesesDelAño(array) {
+  //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
+  // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
+  //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
+  // Tu código:
+  let mesesDeseados = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+      mesesDeseados.push(array[i]);
+    }
+  }
+  if (mesesDeseados.length < 3) return "No se encontraron los meses pedidos"
+  return mesesDeseados
+}
+
+function mayorACien(array) {
+  //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
+  //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
+  // Tu código:
+  let contando = array.filter((item)=>item > 100);
+  return contando
+}
+
+// No modificar nada debajo de esta línea, de lo contrario no correrán los test.
+// --------------------------------
+
+module.exports = {
+  devolverPrimerElemento,
+  devolverUltimoElemento,
+  obtenerLargoDelArray,
+  incrementarPorUno,
+  agregarItemAlFinalDelArray,
+  agregarItemAlComienzoDelArray,
+  dePalabrasAFrase,
+  arrayContiene,
+  agregarNumeros,
+  promedioResultadosTest,
+  numeroMasGrande,
+  cuentoElementos,
+  diaDeLaSemana,
+  empiezaConNueve,
+  todosIguales,
+  mesesDelAño,
+  mayorACien,
+};
